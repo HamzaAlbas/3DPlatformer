51a4c3a49d90a685f156c9305feb139b881707c9
Hamza Albaş <hamza.albas@hotmail.com>
2023-10-20 17:43:10 +0300

    Added a teleporter logic.

---
+++ 3DPlatformer/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Transform teleportA;
+    public Transform teleportB;
+
+    public Transform blackoutA;
+    public Transform blackoutB;
+
+    private GameObject playerTransform;
+
+    public void Teleport(Component sender, object data)
+    {
+        playerTransform = (GameObject) data;
+
+        Debug.Log("Start teleporting");
+
+        playerTransform.transform.position = teleportA.position;
+
+        if (playerTransform.TryGetComponent<PlayerMovement>(out var player))
+        {
+            player.canMove = false;
+        }
+        StartCoroutine(StartTeleporting());
+    }
+
+
+    IEnumerator StartTeleporting()
+    {
+        blackoutA.gameObject.SetActive(true);
+        blackoutB.gameObject.SetActive(true);
+        teleportB.gameObject.SetActive(true);
+        playerTransform.transform.position = teleportB.position;
+        yield return new WaitForSeconds(2);
+        blackoutA.gameObject.SetActive(false);
+        blackoutB.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1);
+
+        if (playerTransform.TryGetComponent<PlayerMovement>(out var player))
+        {
+            player.canMove = true;
+        }
+        teleportB.gameObject.SetActive(false);
+    }
+}
